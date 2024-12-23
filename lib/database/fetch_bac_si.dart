183279edf7fceec7474077dc17f62c8bf9afb022
Dang Hoang <hdjhdbsudg@gmail.com>
2024-12-23 09:48:29 +0700

    sang 23-12

--- lib/database/fetch_bac_si.dart
+++ lib/database/fetch_bac_si.dart
@@ -1,6 +1,6 @@
 import 'package:toikhoe/database/fetch_userID_password.dart';
 
-Future<List<Map<String, dynamic>>> fetchAllBacSi() async {
+Future<List<Map<String, dynamic>>> fetchAllDoctors() async {
   List<Map<String, dynamic>> doctors = [];
   if (conn == null) {
     print('Kết nối chưa được khởi tạo.');
@@ -8,12 +8,19 @@ Future<List<Map<String, dynamic>>> fetchAllBacSi() async {
   }
 
   try {
-    // Thực hiện truy vấn để lấy toàn bộ dữ liệu từ bảng BacSi
-    final result = await conn!.query("SELECT * FROM BacSi");
+    // Thực hiện truy vấn để lấy toàn bộ dữ liệu từ bảng Doctors
+    final result = await conn!.query("SELECT * FROM Doctors");
 
     // Lưu trữ dữ liệu từng bản ghi vào danh sách
     for (var row in result) {
-      doctors.add(row.fields);
+      doctors.add({
+        'doctorID': row['doctorID'],
+        'userID': row['userID'],
+        'specialization': row['specialization'],
+        'experience': row['experience'],
+        'working_hours': row['working_hours'],
+        'location': row['location'],
+      });
     }
 
     print('Lấy toàn bộ thông tin bác sĩ thành công: $doctors');
